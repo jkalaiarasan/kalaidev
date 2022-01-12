5939ea21b4b69e212ffc9e8216d759364d3d6286
jkalaiarasan <60134661+jkalaiarasan@users.noreply.github.com>
2022-01-12 20:23:28 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, make_response
+from router.customerRoute import customer
+from router.consumerRoute import consumer
+
+app = Flask(__name__)
+
+app.register_blueprint(customer)
+app.register_blueprint(consumer)
+
+@app.route('/')
+def homePage():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug = True)
