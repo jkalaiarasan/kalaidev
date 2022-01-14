75d6251b6e5b4dac4d213867f0b03568f8436bbc
jkalaiarasan <60134661+jkalaiarasan@users.noreply.github.com>
2022-01-14 07:18:48 +0530

    Remove command

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, make_response
-#from router.customerRoute import customer
-#from router.consumerRoute import consumer
+from router.customerRoute import customer
+from router.consumerRoute import consumer
 
 app = Flask(__name__)
 
-#app.register_blueprint(customer)
-#app.register_blueprint(consumer)
+app.register_blueprint(customer)
+app.register_blueprint(consumer)
 
 @app.route('/')
 def homePage():
